0c16a5739e97b3c362e575fb4c0476498f6c3e30
vaznerd <piyush.vaznerd@gmail.com>
2026-07-18 21:16:59 +0530

    style(auth): format repository struct literals and imports

--- backend/internal/auth/repository.go
+++ backend/internal/auth/repository.go
@@ -1,14 +1,17 @@
 package auth
 
-import "github.com/jackc/pgx/v5/pgxpool"
+import (
+	"github.com/jackc/pgx/v5/pgxpool"
+)
 
-type RefreshTokenRepository interface {
-}
+type RefreshTokenRepository interface{}
 
 type refreshTokenRepository struct {
 	db *pgxpool.Pool
 }
 
 func NewRefreshTokenRepository(db *pgxpool.Pool) RefreshTokenRepository {
-	return &refreshTokenRepository{db: db}
+	return &refreshTokenRepository{
+		db: db,
+	}
 }
